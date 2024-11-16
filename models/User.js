1fc9c34ffaf85f0b3b320ff57932a1373aea3be9
meshach567 <arinzemeshach123@gmail.com>
2024-11-16 17:53:41 +0100

    refactor the model folder and add user.js to main

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true},
+    password: { type: String, required: true},
+    role: { type: String, enum: ['User', 'Admin'], default: 'User'}
+})
+
+module.exports = mongoose.model('User', userSchema);
